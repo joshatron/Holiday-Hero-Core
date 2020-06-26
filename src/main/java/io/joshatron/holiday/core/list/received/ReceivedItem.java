9a29e387e7d8399a3a0049d29ee7ac1f5d2f4c3f
Joshua Leger <legermjoshua@gmail.com>
2020-06-25 21:26:48 -0700

    Made recieved list extend generic list

---
+++ src/main/java/io/joshatron/holiday/core/list/received/ReceivedItem.java
@@ -0,0 +1,9 @@
+package io.joshatron.holiday.core.list.received;
+
+import io.joshatron.holiday.core.list.GenericItem;
+
+public class ReceivedItem extends GenericItem {
+    public ReceivedItem(String id) {
+        super(id);
+    }
+}
--- src/main/java/io/joshatron/holiday/core/list/received/ReceivedList.java
+++ src/main/java/io/joshatron/holiday/core/list/received/ReceivedList.java
@@ -1,6 +1,9 @@
 package io.joshatron.holiday.core.list.received;
 
-public class ReceivedList {
+import io.joshatron.holiday.core.list.GenericList;
+
+public class ReceivedList extends GenericList<ReceivedItem> {
     public ReceivedList(String id, String owner) {
+        super(id, owner);
     }
 }
