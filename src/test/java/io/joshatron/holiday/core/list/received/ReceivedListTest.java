cc11e6b161b0d06ca060c86a89abd5f22d137658
Joshua Leger <legermjoshua@gmail.com>
2020-07-01 21:18:03 -0700

    Added validation to testing

--- src/test/java/io/joshatron/holiday/core/list/received/ReceivedListTest.java
+++ src/test/java/io/joshatron/holiday/core/list/received/ReceivedListTest.java
@@ -1,5 +1,6 @@
 package io.joshatron.holiday.core.list.received;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.UUID;
@@ -7,7 +8,13 @@ import java.util.UUID;
 public class ReceivedListTest {
     @Test
     public void createList() {
-        ReceivedList list = new ReceivedList(randomId(), randomId());
+        String id = randomId();
+        String owner = randomId();
+        ReceivedList list = new ReceivedList(id, owner);
+
+        Assert.assertEquals(list.getId(), id);
+        Assert.assertEquals(list.getOwner(), owner);
+        Assert.assertTrue(list.getList().isEmpty());
     }
 
     private String randomId() {
